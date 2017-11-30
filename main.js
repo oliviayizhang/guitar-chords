6cde9527f26daa5a1581a5f6a17fe909251d2190
oliviayizhang <zhangyi.olivia@gmail.com>
2017-11-29 22:23:11 -0500

    Fixed duplicated clone

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
-  $(".option").draggable({
-    helper: "clone",
-    opacity: 0.5,
-    grid: [50, 20]
-  })
-
-  $("#demo").droppable({
-    accept: ".option",
-    drop: function(event, ui) {
-      if (document.getElementsByTagName("img").length <= 6) {
-        ui.draggable.clone().appendTo($(this))
+    $(".option").draggable({
+      helper: "clone",
+      opacity: 0.5,
+      connectToSortable: "#demo",
+      start: function( event, ui ) {
+        if ($("#demo .option").length >= 3) {
+          event.preventDefault()
+        }
       }
-    }
-  })
+    })
 
-  $("#demo").sortable()
+//can listen to when the event is starting and do stuff
+    // $( ".option" ).on( "dragstart", function( event, ui ) {
+    //   if ($("#demo .option").length >= 3) {
+    //     console.log(ui.helper[0])
+        // $("#demo .option:nth-child(4)").detach();
+    //   }
+    // })
+
+  $("#demo").sortable({
+   axis: "y",
+   items: "> .option",
+   containment: "parent"
+  })
 })
