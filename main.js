755af400d05501315342f8d492443b8f87eca844
oliviayizhang <zhangyi.olivia@gmail.com>
2017-12-05 14:37:41 -0500

    added responsive page design

--- main.js
+++ main.js
@@ -44,16 +44,32 @@ $(document).ready(function() {
     $("#demo .option").remove()
   })
 
+  $("#demo").hover(
+    function() {$(".delete-button").addClass("delete-button-hover")},
+    function() {$(".delete-button").removeClass("delete-button-hover")}
+  )
+
 })
+
+//test
+
 
 // make a function to fill demo with selected music
 
 let fillPage = (id) => {
   $("#demo .option").remove()
-  if($("demo").children().length == 0) {
+  if($("#demo").children('img').length == 0) {
     let times = 3
     for(let i = 0; i < times; i++) {
       $("#demo").append($(id).clone())
     }
   }
+}
+
+let removeOne = (num) => {
+	var count = parseFloat(8)+parseFloat(num);
+	if  (document.getElementsByClassName("ui-draggable").length >= count) {
+  	let element = document.getElementsByClassName("ui-draggable")[num]
+  	element.remove()
+	}
 }
